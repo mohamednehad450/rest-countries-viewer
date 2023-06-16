a8c067387c6e2dd8e9c8d3e96ef3d2636f9fd515
Mohamed Nehad <mohammednehad450@gmail.com>
2023-06-16 13:45:24 +0300

    feat: Add custom colors to tailwindCss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: "class",
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        sans: ["var(--font-nunito)"],
+      },
+      colors: {
+        "dark-blue": "hsl(209, 23%, 22%)", //        Dark Mode Elements
+        "very-dark-blue1": "hsl(207, 26%, 17%)", //  Dark Mode Background
+        "very-dark-blue2": "hsl(200, 15%, 8%)", //   Light Mode Text
+        "dark-gray": "hsl(0, 0%, 52%)", //           Light Mode Input
+        "very-light-gray": "hsl(0, 0%, 98%)", //     Light Mode Background
+        // "white"                                   Dark Mode Text & Light Mode Elements
       },
     },
   },
   plugins: [],
-}
+};
