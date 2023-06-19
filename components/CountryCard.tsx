9b90158b368ecb14501bdb5fa5b1e9539be3b1ba
Mohamed Nehad <mohammednehad450@gmail.com>
2023-06-19 11:29:13 +0300

    feat: Add CountryCard Skeleton

--- components/CountryCard.tsx
+++ components/CountryCard.tsx
@@ -53,4 +53,32 @@ const CountryCard: FC<CountryCardProps> = ({ country, onClick }) => {
   );
 };
 
+const Skeleton: FC = () => {
+  return (
+    <button
+      className={clsx(
+        "w-64 h-80",
+        "rounded-lg",
+        "overflow-hidden",
+        "shadow dark:shadow-none",
+        "bg-white dark:bg-dark-blue",
+        "flex flex-col",
+        "text-start",
+        "transition-all hover:shadow-2xl hover:dark:shadow-none hover:scale-110"
+      )}
+    >
+      <div className="h-40 w-full bg-gray-100 dark:bg-gray-700" />
+      <div className="px-6 pt-6">
+        <div className="flex flex-col pt-2 gap-1">
+          <span className="bg-gray-100 dark:bg-gray-700 h-4 w-full" />
+          <span className="bg-gray-100 dark:bg-gray-700 h-3 w-2/3 mt-4" />
+          <span className="bg-gray-100 dark:bg-gray-700 h-3 w-2/3 my-2" />
+          <span className="bg-gray-100 dark:bg-gray-700 h-3 w-2/3" />
+        </div>
+      </div>
+    </button>
+  );
+};
+
+export { Skeleton };
 export default CountryCard;
